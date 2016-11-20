e35a4418ce36583542ed6111a1ed1cbbc8bd98f7
Yong Choi <sk8er.choi@gmail.com>
2016-11-20 15:56:23 +0900

    add comment

--- yong/20161125-db/wiringPi_led.sh
+++ yong/20161125-db/wiringPi_led.sh
@@ -1,10 +1,13 @@
 #! /bin/sh
 
+# author: Yong Choi <sk8er.choi@gmail.com>
+# You may register this script at /etc/rc.local
+
 PIN=$1
 VALUE=$2
 DIR=/home/pi/UKC-CSharp-RaspberryPi_201610/yong/20161125-db
 
-if [ $# -eq 1 ] && [ -f $DIR/$PIN.on ]; then
+if [ $# -eq 1 ] && [ -f $DIR/$PIN.on ]; then 
     gpio mode $PIN out
     gpio write $PIN 1 
 elif [ $# -eq 2 ]; then
